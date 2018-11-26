4f9a32d8148030f4b5a071edbdc1d98aba90980d
dragocat <37679458+dragocat@users.noreply.github.com>
2018-11-26 12:45:57 -0600

    Update TeleOpMain.java

--- code/TeleOpMain.java
+++ code/TeleOpMain.java
@@ -3,38 +3,40 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="TeleOpMain")
 public class TeleOpMain extends LinearOpMode{
 
-    private DcMotor Motor0; //Front Right Wheel Motor
-    private DcMotor Motor1; //Back Right Wheel Motor
-    private DcMotor Motor2; //Back Left Wheel Motor
-    private DcMotor Motor3; //Front Left Wheel Motor
-	private DcMotor ArmMotor; //Mineral Arm Up/Down Motor
-	private DcMotor FlipMotor; //Mineral Arm Rotation Motor
-	private DcMotor LiftMotor; //Robot Up/Down Motor
+    private DcMotor Motor0;
+    private DcMotor Motor1;
+    private DcMotor Motor2;
+    private DcMotor Motor3;
+    private DcMotor LiftMotor;
+    private DcMotor ArmMotor;
+    private DcMotor FlipMotor;
+    private final double SLOW = .2;
+    private final double NORMAL = .5;
+    private final double FAST = 1;
 
     @Override
     public void runOpMode() throws InterruptedException{
 
         int mode = 0; //0(x): Pickup Minerals, 1(y): Lift Robot
-        double speed = 0.5;
+        double speed = NORMAL;
         Motor0 = hardwareMap.dcMotor.get("Motor0");
         Motor1 = hardwareMap.dcMotor.get("Motor1");
         Motor2 = hardwareMap.dcMotor.get("Motor2");
         Motor3 = hardwareMap.dcMotor.get("Motor3");
         Motor3.setDirection(DcMotor.Direction.REVERSE);
         LiftMotor = hardwareMap.dcMotor.get("LiftMotor");
-		FlipMotor = hardwareMap.dcMotor.get("FlipMotor");
         ArmMotor = hardwareMap.dcMotor.get("ArmMotor");
+        FlipMotor = hardwareMap.dcMotor.get("FlipMotor");
 
         waitForStart();
 
         while (opModeIsActive()) {
 
-            //Drive Code
+            //drive code
             float M1 = gamepad1.left_stick_y - gamepad1.left_stick_x;
             float M2 = gamepad1.left_stick_y + gamepad1.left_stick_x;
 
@@ -56,15 +58,18 @@ public class TeleOpMain extends LinearOpMode{
 
             }
 
-            //Changing Wheel Speeds
+            //Fast/slow modes
             if (gamepad1.dpad_up) {
-                speed = 0.5;
+                speed = FAST;
             }
             else if (gamepad1.dpad_down) {
-                speed = 0.2;
+                speed = SLOW;
+            }
+            else if (gamepad1.dpad_left) {
+                speed = NORMAL;
             }
 
-            //Changing Secondary Arm Modes
+            //change arm mode bw Lifting Arm and Mineral Pickup Arm
             if (gamepad2.x) {
                 mode = 0;
             }
@@ -72,17 +77,17 @@ public class TeleOpMain extends LinearOpMode{
                 mode = 1;
             }
 
-            //Arm Movement Code
+            //Switch between
             if (mode == 0) {
                 //Mineral Pickup arm
-                if (-gamepad2.right_stick_y > 0) {
-                    FlipMotor.setPower(-gamepad2.right_stick_y * .5); //Rotate Up
+                if (gamepad2.right_stick_y > 0) {
+                    FlipMotor.setPower(gamepad2.right_stick_y * .3); //down
                 }
                 else{
-                    FlipMotor.setPower(-gamepad2.right_stick_y * .3); //Rotate Down
+                    FlipMotor.setPower(gamepad2.right_stick_y * .65); //up
                 }
 
-                ArmMotor.setPower(gamepad2.left_stick_y * .2); //Move Arm Up/Down
+                ArmMotor.setPower(gamepad2.left_stick_y * .2);
 
             }
             else {
