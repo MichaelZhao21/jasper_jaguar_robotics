3706664556fbf4a814664ee521e7724f7d2e1179
dragocat <37679458+dragocat@users.noreply.github.com>
2019-01-31 22:25:58 -0600

    Update for encoder support and changed Motor names

--- RoverRuckus/TestCode/JustWheels.java
+++ RoverRuckus/TestCode/JustWheels.java
@@ -4,29 +4,43 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 
+/**
+ * Wheel diagram:
+ *      ___
+ *    0\ F /3
+ *     |   |
+ *    1/_B_\2
+ *
+ */
 @TeleOp(name = "JustWheels")
-
 public class JustWheels extends LinearOpMode{
 
-    public DcMotor Motor1;
-    public DcMotor Motor2;
-    public DcMotor Motor3;
-    public DcMotor Motor4;
+    private DcMotor Motor0;
+    private DcMotor Motor1;
+    private DcMotor Motor2;
+    private DcMotor Motor3;
+    private final double SLOW = 0.2;
+    private final double NORMAL = 0.5;
+    private final double FAST = 0.8;
 
     public void runOpMode() throws InterruptedException{
 
-        int position = 0;
-        boolean other = true;
         double speed = 0.8;
+        Motor0 = hardwareMap.dcMotor.get("Motor0");
         Motor1 = hardwareMap.dcMotor.get("Motor1");
         Motor2 = hardwareMap.dcMotor.get("Motor2");
         Motor3 = hardwareMap.dcMotor.get("Motor3");
-        Motor4 = hardwareMap.dcMotor.get("Motor4");
-
-        Motor2.setDirection(DcMotorSimple.Direction.REVERSE);
+        Motor0.setDirection(DcMotor.Direction.REVERSE);
+        Motor1.setDirection(DcMotor.Direction.REVERSE);
+        Motor0.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Motor3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        Motor0.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Motor1.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        Motor3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
 
@@ -47,19 +61,22 @@ public class JustWheels extends LinearOpMode{
             }
             else {
 
-                Motor1.setPower((M1) * speed);
-                Motor2.setPower((M2) * speed);
-                Motor3.setPower((-M1) * speed);
-                Motor4.setPower((-M2) * speed);
+                Motor0.setPower((M1) * speed);
+                Motor1.setPower((M2) * speed);
+                Motor2.setPower((-M1) * speed);
+                Motor3.setPower((-M2) * speed);
 
             }
 
-            //Fast/slow modes
+            //Speed modes
             if (gamepad1.dpad_down) {
-                speed = 0.8;
+                speed = SLOW;
+            }
+            else if (gamepad1.dpad_left || gamepad1.dpad_right) {
+                speed = NORMAL;
             }
             else if (gamepad1.dpad_up) {
-                speed = 0.4;
+                speed = FAST;
             }
 
         }
@@ -67,10 +84,10 @@ public class JustWheels extends LinearOpMode{
 
     public void pivot(float target, double multiplier){
 
+        Motor0.setPower(target * multiplier);
         Motor1.setPower(target * multiplier);
         Motor2.setPower(target * multiplier);
         Motor3.setPower(target * multiplier);
-        Motor4.setPower(target * multiplier);
 
     }
 }
